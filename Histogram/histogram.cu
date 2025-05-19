1013fa179c078c5a2e273925fd1c76769c7ae9b2
praneet9 <praneetbomma@gmail.com>
2025-05-19 09:12:40 +0100

    completed histogram kernel

--- Histogram/histogram.cu
+++ Histogram/histogram.cu
@@ -1,15 +1,22 @@
 #include <cuda_runtime.h>
 
-__global__ void histogramKernel(int *input, int* output) {
+__global__ void histogramKernel(int *input, int* output, int length, int min_score, int max_score, int n_bins) {
     int idx = blockIdx.x * blockDim.x + threadIdx.x;
-    idx = idx + 2;
-    // Your kernel logic here
+
+    if (idx >= length) return;
+
+    // Formula to bin any range into n_bins:
+    // int bin = min((value - min_value) * num_bins / (max_value - min_val + 1), num_bins - 1)
+    // Note: bin is converted to int from float, so 2.99 becomes 2
+    int bin = min((input[idx] - min_score) * n_bins / (max_score - min_score + 1), n_bins - 1);
+
+    atomicAdd(&(output[bin]), 1);
 }
 
-void launch_histogram_kernel(int* input_ptr, int* output_ptr, int length) {
+void launch_histogram_kernel(int* input_ptr, int* output_ptr, int length, int min_score, int max_score, int n_bins) {
 
     int threads = 256;
     int blocks = (length + threads - 1) / threads;
 
-    histogramKernel<<<blocks, threads>>>(input_ptr, output_ptr);
+    histogramKernel<<<blocks, threads>>>(input_ptr, output_ptr, length, min_score, max_score, n_bins);
 }
--- Histogram/main.cpp
+++ Histogram/main.cpp
@@ -1,15 +1,25 @@
 #include <torch/torch.h>
 #include <iostream>
 
-void launch_histogram_kernel(int* input_ptr, int* output_ptr, int length);  // declare it
+void launch_histogram_kernel(int* input_ptr, int* output_ptr, int length, int min_score, int max_score, int n_bins);  // declare it
 
 int main() {
     int length = 1024;
+    int min_score = 0;
+    int max_score = 1000;
+    int n_bins = 10;
 
-    at::Tensor input_tensor = torch::randint(0, 1000, {length}, torch::dtype(torch::kInt32).device(torch::kCUDA));
-    at::Tensor output_tensor = torch::zeros({10}, torch::dtype(torch::kInt32).device(torch::kCUDA));
+    at::Tensor input_tensor = torch::randint(min_score, max_score, {length}, torch::dtype(torch::kInt32).device(torch::kCUDA));
+    at::Tensor output_tensor = torch::zeros({n_bins}, torch::dtype(torch::kInt32).device(torch::kCUDA));
 
-    launch_histogram_kernel(input_tensor.data_ptr<int>(), output_tensor.data_ptr<int>(), input_tensor.size(0));
+    launch_histogram_kernel(input_tensor.data_ptr<int>(), output_tensor.data_ptr<int>(), input_tensor.size(0), min_score, max_score, n_bins);
 
-    std::cout << input_tensor.cpu() << std::endl;
+    std::cout << "CUDA Kernel output:\n" << output_tensor.cpu() << std::endl;
+    auto output = torch::histogram(input_tensor.toType(torch::kFloat32).cpu(), n_bins);
+
+    at::Tensor bin_counts = std::get<0>(output);
+    at::Tensor bin_edges  = std::get<1>(output);
+
+    std::cout << "\nTorch output: \n" << "Bin counts:\n" << bin_counts << "\n";
+    std::cout << "Bin edges:\n"  << bin_edges  << "\n";
 }
